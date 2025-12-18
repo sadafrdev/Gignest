461395903d3f44dc8309b15093c6d49571de11f3
Sadaf Raza <sadaf.r.dev@gmail.com>
2025-12-18 06:20:31 +0500

    add migration

---
+++ api/migrations/20251212135354_init.sql
@@ -0,0 +1,16 @@
+-- Add migration script here
+
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  first_name TEXT,
+  last_name TEXT,
+  username TEXT UNIQUE,
+  phone_number TEXT,
+  country TEXT,
+  picture_url TEXT,
+  role TEXT NOT NULL DEFAULT 'user',
+  created_at: TEXT
+  created_at TIMESTAMPTZ NOT NULL
+);
